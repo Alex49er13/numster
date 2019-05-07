41f3e828cf45e952d89a3fdcf961427b7b5e09d0
sierra mills <Alex49er13@gmail.com>
2019-05-07 23:41:00 +0000

    edit capability

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -13,6 +13,14 @@ before_action :authenticate_user!, only: [:new, :create]
     redirect_to root_path
   end
 
+  def show
+    @place = Place.find(params[:id])
+  end
+
+  def edit
+    @place = Place.find(params[:id])
+  end
+
   private
 
   def place_params
