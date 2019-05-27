24e2565d0e616820df79809fedec325a7785ff3f
sierra mills <Alex49er13@gmail.com>
2019-05-27 04:38:14 +0000

    user dashboard page

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,9 @@ Rails.application.routes.draw do
   root 'places#index'
   resources :places do 
     resources :comments, :photos, only: :create
+
   end
-   
+  resources :users, only: :show 
 end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
